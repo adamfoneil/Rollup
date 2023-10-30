e66eeb7bf831e7c9b7f9eaf105c725c105831c34
Adam O'Neil <adamosoftware@gmail.com>
2023-10-30 12:48:36 -0400

    comments

--- Rollup.Tests/Entities/SalesRollup.cs
+++ Rollup.Tests/Entities/SalesRollup.cs
@@ -6,6 +6,9 @@ internal class SalesRollup
 	public string Region { get; set; } = default!;
 	public string ItemType { get; set; } = default!;
 	public int Year { get; set; }
+	/// <summary>
+	/// this is the "fact" column in the rollup
+	/// </summary>
 	public decimal Total { get; set; }
 
 	public static implicit operator SalesRollupKey(SalesRollup entity) => new()
@@ -16,6 +19,9 @@ internal class SalesRollup
 	};
 }
 
+/// <summary>
+/// this is, in effect, your "dimension" columns
+/// </summary>
 internal record SalesRollupKey
 {
 	public string Region { get; set; } = default!;
