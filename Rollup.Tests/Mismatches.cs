b311c7b51d76f511db230b6f83df5e75285bd5e0
Adam O'Neil <adamosoftware@gmail.com>
2023-11-04 09:17:11 -0400

    mismatch finder

---
+++ Rollup.Tests/Mismatches.cs
@@ -0,0 +1,64 @@
+﻿using RollupLibrary;
+using System.Data;
+
+namespace Rollup.Tests;
+
+[TestClass]
+public class Mismatches
+{
+	[TestMethod]
+	public async Task FindMismatches()
+	{
+		// this is not needed for the test, but MismatchFinder requires a connection arg
+		using var cn = Util.InitDatabase(
+			"Rollup.Tests.Resources.RollupDemo.bacpac",
+			"Server=(localdb)\\mssqllocaldb;Database=RollupDemo;Integrated Security=true");
+
+		var finder = new SampleMismatchFinder();
+		var result = await finder.QueryAsync(cn);
+
+		Assert.IsTrue(result.Count() == 2);
+		Assert.IsTrue(result.Any(item => item.Type == MismatchType.NotInRollup && item.Data.Id == 6 && item.Data.Value == "Grouper"));
+		Assert.IsTrue(result.Any(item => item.Type == MismatchType.NotInTransactional && item.Data.Id == 3 && item.Data.Value == "Whatever"));
+	}
+}
+
+internal class SampleData
+{
+	public int Id { get; set; }
+	public string Value { get; set; } = default!;
+}
+
+
+internal class SampleMismatchFinder : MismatchFinder<SampleData, int>
+{
+	protected override int GetIdentity(SampleData result) => result.Id;
+	
+	protected override async Task<IEnumerable<SampleData>> QueryRollupAsync(IDbConnection connection)
+	{
+		await Task.CompletedTask;
+
+		return new SampleData[]
+		{
+			new() { Id = 1, Value = "Hello" },
+			new() { Id = 2, Value = "Goodbye" },
+			new() { Id = 3, Value = "Whatever" },
+			new() { Id = 4, Value = "Astyanax" },
+			new() { Id = 5, Value = "Ozymandias" },
+		};
+	}
+
+	protected override async Task<IEnumerable<SampleData>> QueryTransactionalAsync(IDbConnection connection)
+	{
+		await Task.CompletedTask;
+
+		return new SampleData[]
+		{
+			new() { Id = 1, Value = "Hello" },
+			new() { Id = 2, Value = "Goodbye" },			
+			new() { Id = 4, Value = "Astyanax" },
+			new() { Id = 5, Value = "Ozymandias" },
+			new() { Id = 6, Value = "Grouper" }
+		};
+	}
+}
---
+++ Rollup/MismatchFinder.cs
@@ -0,0 +1,37 @@
+﻿using System.Data;
+
+namespace RollupLibrary;
+
+public enum MismatchType
+{
+	NotInTransactional,
+	NotInRollup
+}
+
+/// <summary>
+/// defines a way to find mismatched detail rows between two report queries,
+/// used for troubleshooting rollups
+/// </summary>
+public abstract class MismatchFinder<TResult, TIdentity> 
+	where TIdentity : struct 
+	where TResult : notnull
+{
+	protected abstract TIdentity GetIdentity(TResult result);
+
+	protected abstract Task<IEnumerable<TResult>> QueryTransactionalAsync(IDbConnection connection);
+
+	protected abstract Task<IEnumerable<TResult>> QueryRollupAsync(IDbConnection connection);
+
+	public async Task<IEnumerable<(MismatchType Type, TResult Data)>> QueryAsync(IDbConnection connection)
+	{
+		List<(MismatchType, TResult)> results = new();
+
+		var transactionalDetail = (await QueryTransactionalAsync(connection)).ToDictionary(GetIdentity);
+		var rollupDetail = (await QueryRollupAsync(connection)).ToDictionary(GetIdentity);
+
+		results.AddRange(transactionalDetail.Keys.Except(rollupDetail.Keys).Select(id => (MismatchType.NotInRollup, transactionalDetail[id])));
+		results.AddRange(rollupDetail.Keys.Except(transactionalDetail.Keys).Select(id => (MismatchType.NotInTransactional, rollupDetail[id])));
+
+		return results;
+	}
+}
